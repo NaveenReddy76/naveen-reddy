b2f92b4bd82289aa73c58322383949615780324d
NAVEEN KUMAR REDDY <harinath.py123@gmail.com>
2022-09-10 11:58:08 +0530

    message

---
+++ day 1 list/index.py
@@ -0,0 +1,8 @@
+my_world=[4,6,8,2.2,"a","x","y","true",4,2.2,"a"]
+print(len(my_world))
+print(my_world[5])
+print(my_world[1])
+print(my_world[10])
+print(my_world[-8])
+print(my_world[3])
+print(my_world[3],my_world[5])
---
+++ string_data.py
@@ -0,0 +1,2 @@
+my_world =[4,6,8,2.2,"a","x","y","true",4,2.2,"a"]
+print(len(my_world))
